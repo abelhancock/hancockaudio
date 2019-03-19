fa03ae285b6a5f213f2f2fab05f37ea7c749d787
Abel <abel.hancock@liferay.com>
2019-03-18 23:35:03 -0700

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
         background_color: `#ECE2D4`,
         theme_color: `#ECE2D4`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- gatsby-node.js
+++ gatsby-node.js
@@ -12,12 +12,3 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
     })
   }
 }
-
-//absolute imports
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      modules: [path.resolve(__dirname, "src"), "node_modules"],
-    },
-  })
-}
