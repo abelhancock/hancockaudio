cb00e101155d8031a569fd39bafaf26e3b1e97f4
Abel <abel.hancock@liferay.com>
2019-03-18 23:21:26 -0700

    still fixing document not defined error

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,23 @@
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-    if (stage === "build-html") {
-      actions.setWebpackConfig({
-        module: {
-          rules: [
-            {
-              test: /bad-module/,
-              use: loaders.null(),
-            },
-          ],
-        },
-      })
-    }
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: "./src/components/NewContactForm",
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
   }
+}
+
+//absolute imports
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
+    },
+  })
+}
