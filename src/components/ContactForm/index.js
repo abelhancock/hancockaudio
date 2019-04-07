298d11df41f9bcc536cd562a63a4f26448cf48bd
Abel <abel.hancock@liferay.com>
2019-04-06 18:53:40 -0700

    add hidden name input to form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -41,49 +41,57 @@ class ContactForm extends Component {
                 {({ ref, proximity }) => (
                   <ul>
                     <li>
-                          <input
-                              type="hidden"
-                              name="bot-field"
-                              />
+                      <input 
+                          type="hidden"
+                          name="form-name"
+                          value="Contact" />
+                    </li>
+                    <li style={{display: 'none'}}>
+                      <label>
+                        Don't fill this out
+                        <input
+                            name="bot-field"
+                            />
+                      </label>
+                    </li>
+                    <li>
+                        <Input
+                            proximity={proximity}
+                            type="text"
+                            name="Name"
+                            id="name"
+                            required
+                            />
+                        <label htmlFor="name">Name</label>
+                    </li>
+                    <li>
+                        <Input
+                            proximity={proximity}
+                            type="email"
+                            name="Email"
+                            id="email"
+                            required
+                            />
+                          <label htmlFor="email">Email</label>
                       </li>
                       <li>
-                          <Input
-                              proximity={proximity}
-                              type="text"
-                              name="Name"
-                              id="name"
-                              required
-                              />
-                          <label htmlFor="name">Name</label>
-                      </li>
-                      <li>
-                          <Input
-                              proximity={proximity}
-                              type="email"
-                              name="Email"
-                              id="email"
-                              required
-                              />
-                              <label htmlFor="email">Email</label>
-                      </li>
-                      <li>
-                          <textarea
-                              name="Message"
-                              id="message"
-                              >
-                          </textarea>
-                          <label htmlFor="message">Message</label>
+                        <textarea
+                            name="Message"
+                            id="message"
+                            >
+                        </textarea>
+                        <label htmlFor="message">Message</label>
                       </li>
                       <li data-type="control_button">
-                          <button
-                              ref={ref}
-                              type="submit"
-                              id="submit"
-                              > 
-                              Submit <svg viewBox="0 0 31 13" xmlns="http://www.w3.org/2000/svg">
+                        <button
+                            ref={ref}
+                            type="submit"
+                            id="submit"
+                            > 
+                            Submit <svg viewBox="0 0 31 13" xmlns="http://www.w3.org/2000/svg">
 <path d="M25.1113 5.00195C24.1465 3.85091 23.1393 2.18359 22.0898 0H23.918C26.0677 2.53906 28.3529 4.41797 30.7734 5.63672V6.55078C28.3529 7.76953 26.0677 9.64844 23.918 12.1875H22.0898C23.1393 10.0039 24.1465 8.33659 25.1113 7.18555H0V5.00195H25.1113Z" fill="currentColor"/>
 </svg>
-                          </button>
+                        </button>
                       </li>
                   </ul>
                   )}
